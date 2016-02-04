853a04af3140ce9bab9cc377f231c7e22b5f1f8d
shmilky <shmulik@propcy.com>
2016-02-04 12:03:02 +0200

    A better html on extension button hover A minifyed js file A new version of jquery React to mouse enter and leave instead of hover

--- pageContent.js
+++ pageContent.js
@@ -1,37 +1,43 @@
-var whisiButton = '<span class="wishiBtn" title="Add item to your Wishi closet"></span>';
+var wishiButton = "<a class=\"wishiBtn\" title=\"Add item to your Wishi closet\" target=\"_blank\"></a>";
 
 var addWishiItButtons = function() {
-    $('img').each(function() {
+    $("img").mouseenter(function() {
         var imgElement = $(this);
-        if(imgElement.find('wishiBtn').length == 0) {
-            var newButton = $(whisiButton);
-            newButton.css( "top", imgElement.position().top + 10 + 'px' );
-            newButton.css( "left", imgElement.position().left + 10 + 'px' );
-            newButton.attr("imgSrc", imgElement.attr("src"));
-            newButton.insertAfter(imgElement);
-            //imgElement.parent().append(newButton);
+        var newButton;
+        var wishiBtnArr = imgElement.parent().find(".wishiBtn");
 
-            imgElement.hover(function() {
-                if (imgElement.width() > 100 && imgElement.height() > 100) {
-                    newButton.css("display", "block");
-                }
-            }, function() {
-                newButton.css( "display", "none");
-            });
-
-            newButton.click(function() {
-                window.open(newButton.attr('imgSrc'));
-            });
-            //imgElement.append(newButton);
+        // Checking if there is no wishi button and if so it'll create a new one if exists it'll find it and set it as the newButton
+        if(wishiBtnArr.length == 0) {
+            newButton = $(wishiButton);
         }
+        else {
+            newButton = $(wishiBtnArr[0]);
+        }
+
+        // Set the position of the wishi button
+        newButton.css("top", imgElement.position().top + 10 + "px");
+        newButton.css("left", imgElement.position().left + 10 + "px");
+
+        // Will display the button only if the image is big enough
+        if (imgElement.width() > 100 && imgElement.height() > 100) {
+            newButton.css("display", "block");
+        }
+
+        // Will set the link to the according to the current image
+        newButton.attr("href", imgElement.attr("src"));
+
+        if (wishiBtnArr.length == 0) {
+            newButton.insertAfter(imgElement);
+        }
+    }).mouseleave(function() {
+        // Will hide the button when leaving the image element.
+        $(this).parent().find(".wishiBtn").css( "display", "none");
     });
 };
 
 $(document).ready(function() {
-    window.setTimeout(addWishiItButtons, 2000);
+    window.setTimeout(addWishiItButtons, 1000);
+    $(window).on("hashchange", function() {
+        window.setTimeout(addWishiItButtons, 1000);
+    });
 });
-
-$(window).hashchange(function(){
-    window.setTimeout(addWishiItButtons, 2000);
-});
-
