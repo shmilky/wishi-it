e8fac0a0774658406e75af90120a60ecfe730ef9
shmilky <shmulik@propcy.com>
2016-02-15 19:19:11 +0200

    Unbind old events Presenting the wishi button only when the link is valid (very basic check) Checking for existence of the srcset tag and using it if possible, instead of the regular src tag.

--- pageContent.js
+++ pageContent.js
@@ -1,25 +1,43 @@
 var wishiButton = "<a id=\"wishiBtn\" title=\"Add item to your Wishi closet\" target=\"_blank\"></a>";
 
 var addWishiItButtons = function() {
-    $("img").mouseenter(function() {
+    // First we'll start by unbinding old wishiEvents and only then we'll add the new events.
+    $("img").unbind('mouseenter.wishiEvent mouseleave.wishiEvent').on('mouseenter.wishiEvent', function() {
         var imgElement = $(this);
         var newButton = $('#wishiBtn');
 
-        // Set the position of the wishi button
-        newButton.css("top", imgElement.offset().top + 10 + "px");
-        newButton.css("left", imgElement.offset().left + 10 + "px");
+        // A var for the picture url to be stored in.
+        var pictureUrl;
 
-        // Will display the button only if the image is big enough
-        if (imgElement.width() > 100 && imgElement.height() > 100) {
-            newButton.css("display", "block");
+        // We'll try to check if the img has a valid srcset vars for the img src
+        var imgSrcsetAtrr = imgElement.attr('srcset');
+        if (!!imgSrcsetAtrr) {
+            var srcsetAsStrArr = imgSrcsetAtrr.trim().split(' ');
+            if (srcsetAsStrArr.length > 0) {
+                pictureUrl = encodeURIComponent(fixImgSrcUrl(srcsetAsStrArr[0]));
+            }
         }
 
-        var linkUrl = encodeURIComponent(fixImgSrcUrl(imgElement.attr("src")));
+        // In case we don't have srcset (probably most cases will be like that)
+        if (!pictureUrl) {
+            pictureUrl = encodeURIComponent(fixImgSrcUrl(imgElement.attr("src")));
+        }
 
-        // Will set the link to the according to the current image
-        newButton.attr("href", "http://www.wishi.me/app/#/landing/addToCloset?picture_url=" + linkUrl + '&pageUrl=' + encodeURIComponent(window.location.href));
+        // Only if we have a valid picture url the wishi button will appear
+        if (pictureUrl && pictureUrl.length > 7) {
+            // Set the position of the wishi button
+            newButton.css("top", imgElement.offset().top + 10 + "px");
+            newButton.css("left", imgElement.offset().left + 10 + "px");
 
-    }).mouseleave(function() {
+            // Will display the button only if the image is big enough
+            if (imgElement.width() > 100 && imgElement.height() > 100) {
+                newButton.css("display", "block");
+            }
+
+            // Will set the link to the according to the current image
+            newButton.attr("href", "http://www.wishi.me/app/#/landing/addToCloset?picture_url=" + pictureUrl + '&pageUrl=' + encodeURIComponent(window.location.href));
+        }
+    }).on('mouseleave.wishiEvent', function() {
         // Will hide the button when leaving the image element.
         $("#wishiBtn").css( "display", "none");
     });
@@ -34,22 +52,21 @@ var addWishiItButtons = function() {
             }
 
             if(url[0] == '/' && url[1] == '/') {
-                return 'http:' + url;
+                return window.location.protocol + url;
             }
 
             if (url[0] == '/') {
-                return 'http://' + window.location.host + window.location.pathname + url.substr(1, url.length - 1);
+                return window.location.host.href + '/' + url.substr(1, url.length - 1);
             }
 
-            if (url.substr(0, 2) == './' || url.substr(0, 3) == '../') {
-                return 'http://' + window.location.host + window.location.pathname + url;
-            }
+            return window.location.host.href + '/' + url;
         }
 
-        return url;
+        return false;
     };
 };
 
+// When the document is ready we'll run our code.
 $(document).ready(function() {
     $('body').append($(wishiButton));
     window.setTimeout(addWishiItButtons, 1000);
@@ -58,6 +75,7 @@ $(document).ready(function() {
     });
 });
 
+// For every scroll we'll run our code once again in case there is an infinite scroll.
 $(window).scroll(function() {
     window.setTimeout(addWishiItButtons, 1000);
 });
