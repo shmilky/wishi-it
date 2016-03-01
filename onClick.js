7767fec90cce1abeba663d66639d0fc2f866cc37
shmilky <shmulik@propcy.com>
2016-03-01 03:23:43 +0200

    Remove not needed comments and logs

--- onClick.js
+++ onClick.js
@@ -1,17 +1,5 @@
-//(function(chrome) {
-//    console.log('clicked on - ' + new Date());
-//
-//    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-//        chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
-//            if (response) {
-//                console.log(response.farewell);
-//            }
-//        });
-//    });
-//}(chrome));
-chrome.browserAction.onClicked.addListener(function (tab) { //Fired when User Clicks ICON
-    console.log('clicked on - ' + new Date());
-
+//We'll send a message to the content script that the icon was clicked
+chrome.browserAction.onClicked.addListener(function () { //Fired when User Clicks ICON
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, {message: "start wishi extension"}, function(response) {
             if (response) {
